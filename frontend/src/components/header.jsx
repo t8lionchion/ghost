e0d4d5e3c8a041be76652d70c5309d68c78a1b1b
Ted <tygfuidseujzx@gmail.com>
2025-08-02 15:43:25 +0800

    feat(navbar):初步添加header元件

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,13 @@
+"use client"
+export function Header(){
+    return(
+        <header>
+            <h1>Ghost Move</h1>
+            <p>
+                每個標記，都是一段無法解釋的真相。<br/>
+            全台最陰路線，等你來解鎖。<br/>
+            你的腳步，就是下個傳說的起點。
+            </p>
+        </header>
+    )
+}
