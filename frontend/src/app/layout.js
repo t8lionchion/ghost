ecfcc71ff4695fb7823ee91c7485bd435696f9b6
Ted <tygfuidseujzx@gmail.com>
2025-08-02 17:03:23 +0800

    feat(global.css):css比例異常問題修復

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import BootstrapClient from '../providers/Bootstrap';
 import {Navbar} from '../components/navbar';
