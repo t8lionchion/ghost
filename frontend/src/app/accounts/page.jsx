d064be0449f8f004ef590d19b0b02b396eed4036
Ted <tygfuidseujzx@gmail.com>
2025-08-04 15:53:57 +0800

    feat(login):初步切割好登入頁面的UI

--- frontend/src/app/accounts/page.jsx
+++ frontend/src/app/accounts/page.jsx
@@ -1,7 +1,10 @@
+import {Register} from "./register"
+import { Header } from "../../components/header"
 export function Account(){
     return(
         <>
-            <h1>個人頁面</h1>
+            <Header />
+            <Register />
         </>
     )
 }
