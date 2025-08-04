cedae812e71a369f6ee248cef503d0425b13d6bd
Ted <tygfuidseujzx@gmail.com>
2025-08-04 15:21:51 +0800

    feat(header):改變header名字

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,7 +2,7 @@
 export function Header(){
     return(
         <header>
-            <h1>Ghost Move</h1>
+            <h1>幽約地圖</h1>
             <p>
                 每個標記，都是一段無法解釋的真相。<br/>
             全台最陰路線，等你來解鎖。<br/>
