766f63f580b8cb43dac6fd1287cc22ce488bc62b
Ted <tygfuidseujzx@gmail.com>
2025-08-05 10:56:13 +0800

    hotfix(backend):修復後端無法啟動錯誤

--- backend/ghostmove/settings.py
+++ backend/ghostmove/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9opj$ik97d!36yxbau@ik5$unwe%#i0)(nh@rgy4#3h7o-s5b'
+SECRET_KEY = 'django-insecure-w)^o8gc-ux285h%n5loqfzl13e5@e6c%_d77+kuq9-@7ba^$a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
