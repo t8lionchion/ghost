3576545dda1170665d95e3c6615c30bdae28896f
Ted <tygfuidseujzx@gmail.com>
2025-08-05 09:43:41 +0800

    fear(registers):註冊路由建立

---
+++ frontend/src/app/registers/page.jsx
@@ -0,0 +1,11 @@
+import {Register} from "./register"
+import { Header } from "../../components/header"
+export function Account(){
+    return(
+        <>
+            <Header />
+            <Register />
+        </>
+    )
+}
+export default Account
