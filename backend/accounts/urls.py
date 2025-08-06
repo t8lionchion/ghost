12a807c0519a39f9dc3f18ec75efea2fea52cb4f
Ted <tygfuidseujzx@gmail.com>
2025-08-06 10:57:19 +0800

    feat(accounts):完成登入的jwt機制

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,14 @@
 # accounts/urls.py
 from django.urls import path
 from .views import UsersRegisterView
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', UsersRegisterView.as_view(), name='user-register'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 更新 token
+    path('login/', MyTokenObtainPairView.as_view(), name='my_token_obtain_pair'),
 ]
