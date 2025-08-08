69777cacd27236b6b672fc3376378e6cf8862003
Ted <tygfuidseujzx@gmail.com>
2025-08-09 07:25:43 +0800

    feat(events):取得指定活動id的api進行jwt保護

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import GetActivity_fromView
-
+from .views import GetAllActivityView
 urlpatterns = [
     path('GetActivity_from/<int:id>/', GetActivity_fromView.as_view(), name='GetActivity_from'),
+    path('GetAllActivityView/',GetAllActivityView.as_view(),name='GetAllActivityView')
 ]
--- backend/events/views.py
+++ backend/events/views.py
@@ -6,10 +6,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from events.models import Activity_Form
 from django.http import Http404
+from events.serializers import GetAllActivitySerializers
+from rest_framework.decorators import permission_classes,authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
 
 class GetActivity_fromView(APIView):
     def get(self,request,id,format=None):
+        authentication_classes = [JWTAuthentication]
+        permission_classes = [IsAuthenticated]
         #print("Activity_Form",Activity_Form.objects.filter(pk=id).values("id"))
         try:
             obj=Activity_Form.objects.get(pk=id)
@@ -17,4 +23,7 @@ class GetActivity_fromView(APIView):
             raise Http404("找不到指定活動")
         serializer=GetActivity_fromSerializers(obj)
         return Response(serializer.data,status=status.HTTP_200_OK)
-        
+
+class GetAllActivityView(generics.ListAPIView):
+    queryset=Activity_Form.objects.all()
+    serializer_class=GetAllActivitySerializers
