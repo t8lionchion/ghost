30bb58ce8c66198de61838219e8807623a90578b
Ted <tygfuidseujzx@gmail.com>
2025-08-12 17:10:39 +0800

    feat(events):新增serializers的address

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -5,9 +5,10 @@ class GetAllActivitySerializers(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
     Activity_name=serializers.CharField(read_only=True)
     descripe=serializers.CharField(read_only=True)
+    address=serializers.CharField(read_only=True)
     get_activity_form=serializers.SerializerMethodField()
     def get_activity_form(self,obj):
-        return obj.id ,obj.Activity_name,obj.descripe
+        return obj.id ,obj.Activity_name,obj.descripe,obj.address
 
 
 class GetActivity_fromSerializers(serializers.ModelSerializer):
